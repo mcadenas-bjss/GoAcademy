88bd4a2fcea61bbfd050e458325bfc53b8b1f2da
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2024-08-21 09:36:17 +0100

    templating

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mcadenas-bjss/GoAcademy
 
 go 1.22.5
 
-require github.com/bearbin/go-age v0.0.0-20210220235509-f0fa00c278ce
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958
+	github.com/bearbin/go-age v0.0.0-20210220235509-f0fa00c278ce
+)
---
+++ goWithTests/templating/templating.go
@@ -0,0 +1,56 @@
+package templating
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+
+	"github.com/mcadenas-bjss/GoAcademy/goWithTests/files/post"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p post.Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []post.Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
+		return err
+	}
+
+	return nil
+}
